19574d57f0a60e207c45cb4d328edc4cd84d0edb
Mark Zhu <markz@orionhealth.com>
2021-05-14 10:51:09 +1200

    test if we need to run aws s3 website to host the website every time we sync s3 bucket

--- src/components/auth/LoginView.tsx
+++ src/components/auth/LoginView.tsx
@@ -124,9 +124,7 @@ const LoginView: FC<LoginViewProps> = ({}: LoginViewProps) => {
         <Zoom timeout={500} in={true}>
           <Paper className={classes.paper}>
             <Typography component="h1" variant="h5">
-              App developed and deployed with ci on circleci and cd on aws (ecr
-              ecs blue-green deployment) fully docker containerized with load
-              balancing
+              App developed and deployed with ci on circleci and cd to s3 bucket
             </Typography>
 
             <form className={classes.form} noValidate>
