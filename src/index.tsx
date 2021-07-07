3e5530f623eb8a1c303e69ef993a66fe3a247e43
Mark Zhu <markz@orionhealth.com>
2021-07-07 12:39:45 +1200

    no message

--- src/index.tsx
+++ src/index.tsx
@@ -4,4 +4,5 @@ import App from "./App";
 import { wbInstance } from "./workboxWindow";
 // register service worker using workbox-window.
 wbInstance && wbInstance.register();
+console.log(wbInstance);
 ReactDOM.render(<App />, document.getElementById("root"));
